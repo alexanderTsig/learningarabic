ba4ea8f5c520af9611ded67e5067cfda7d3aab69
Alexander <alexanderTsig.@gmail.com>
2015-04-20 06:37:12 +0800

    inital upload

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -38,7 +38,7 @@ class ComposerAutoloaderInitee310975ccacef8143ccf367bcf2d8de
 
         $loader->register(true);
 
-        require $vendorDir . '/kriswallsmith/Assetic/Asset/src/functions.php';
+        require $vendorDir . '/kriswallsmith/assetic/src/functions.php';
 
         return $loader;
     }
